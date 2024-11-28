8333a47670f0c67cbbdbe0901cc80729411995b1
Ioan Mazurca <mazurcaionut99@gmail.com>
2024-11-28 22:40:54 +0000

    Add a completed UI for the shopping list component

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -3,9 +3,10 @@ import { theme } from "../theme";
 
 interface ShoppingListItemProps {
     name?: string;
+    isCompleted?: boolean;
 }
 
-export function ShoppingListItem({ name }: ShoppingListItemProps) {
+export function ShoppingListItem({ name, isCompleted }: ShoppingListItemProps) {
     const handleDelete = () => {
         Alert.alert(`Are you sure you want to delete ${name}?`, "It will be gone for good", [
             {
@@ -18,9 +19,15 @@ export function ShoppingListItem({ name }: ShoppingListItemProps) {
     };
 
     return (
-        <View style={styles.itemContainer}>
-            <Text style={styles.itemText}>{name}</Text>
-            <TouchableOpacity style={styles.button} onPress={handleDelete} activeOpacity={0.8}>
+        <View style={[styles.itemContainer, isCompleted ? styles.completedContainer : undefined]}>
+            <Text style={[styles.itemText, isCompleted ? styles.completedText : undefined]}>
+                {name}
+            </Text>
+            <TouchableOpacity
+                style={[styles.button, isCompleted ? styles.completedButton : undefined]}
+                onPress={handleDelete}
+                activeOpacity={0.8}
+            >
                 <Text style={styles.buttonText}>Delete</Text>
             </TouchableOpacity>
         </View>
@@ -37,16 +44,28 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "space-between",
     },
+    completedContainer: {
+        backgroundColor: theme.colorLightGrey,
+        borderBottomColor: theme.colorLightGrey,
+    },
     itemText: { fontSize: 18, fontWeight: "200" },
     button: {
         backgroundColor: theme.colorBlack,
         padding: 8,
         borderRadius: 6,
     },
+    completedButton: {
+        backgroundColor: theme.colorGrey,
+    },
     buttonText: {
         color: theme.colorWhite,
         fontWeight: "bold",
         textTransform: "uppercase",
         letterSpacing: 1,
+    },
+    completedText: {
+        textDecorationLine: "line-through",
+        textDecorationColor: theme.colorGrey,
+        color: theme.colorGrey,
     },
 });
