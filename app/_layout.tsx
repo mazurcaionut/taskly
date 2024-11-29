b08004b10e42a13673e0ffb0dd76e6f324ba4987
Ioan Mazurca <mazurcaionut99@gmail.com>
2024-11-29 00:05:47 +0000

    Modal navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,15 @@ export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+            <Stack.Screen
+                name="counter"
+                options={{
+                    title: "Counter",
+                    presentation: "modal",
+                    animation: "slide_from_bottom",
+                }}
+            />
+            <Stack.Screen name="idea" options={{ title: "Idea", presentation: "modal" }} />
         </Stack>
     );
 }
