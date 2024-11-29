34031849c65dc0c74971f7d08f4d1fff71d3a142
Ioan Mazurca <mazurcaionut99@gmail.com>
2024-11-29 21:04:48 +0000

    Persist shopping list in async storage

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { StyleSheet, TextInput, ScrollView, FlatList, View, Text } from "react-native";
+import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getFromStorage, saveToStorage } from "../utils/storage";
+
+const storageKey = "shopping-list";
 
 type ShoppingListItemType = {
     id: string;
@@ -14,6 +17,18 @@ export default function App() {
     const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
     const [value, setValue] = useState("");
 
+    useEffect(() => {
+        const fetchInitial = async () => {
+            const data = await getFromStorage(storageKey);
+
+            if (data) {
+                setShoppingList(data);
+            }
+        };
+
+        fetchInitial();
+    }, []);
+
     const handleSubmit = () => {
         if (value) {
             const newShoppingList = [
@@ -21,12 +36,14 @@ export default function App() {
                 ...shoppingList,
             ];
             setShoppingList(newShoppingList);
+            saveToStorage(storageKey, newShoppingList);
             setValue("");
         }
     };
 
     const handleDelete = (id: string) => {
         const newShoppingList = shoppingList.filter((item) => item.id !== id);
+        saveToStorage(storageKey, newShoppingList);
         setShoppingList(newShoppingList);
     };
 
@@ -43,6 +60,7 @@ export default function App() {
             return item;
         });
 
+        saveToStorage(storageKey, newShoppingList);
         setShoppingList(newShoppingList);
     };
 
