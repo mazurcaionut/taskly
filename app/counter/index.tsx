420796b776ffbf48e33600cf07eb376d2067e56a
Ioan Mazurca <mazurcaionut99@gmail.com>
2024-11-29 23:51:05 +0000

    Implemented history page with past completions

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -10,9 +10,9 @@ import { getFromStorage, saveToStorage } from "../../utils/storage";
 
 const frequency = 10 * 1000;
 
-const countdownStorageKey = "taskly-countdown";
+export const countdownStorageKey = "taskly-countdown";
 
-type PersistedCountdownState = {
+export type PersistedCountdownState = {
     currentNotificationId: string | undefined;
     completedAtTimestamps: number[];
 };
