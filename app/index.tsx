3f0dc3497ee0f4f52a413dfec358b34bd186a0af
Ioan Mazurca <mazurcaionut99@gmail.com>
2024-11-29 00:23:37 +0000

    Set up bottom tabs navigation

--- app/index.tsx
+++ app/index.tsx
@@ -6,10 +6,6 @@ import { Link } from "expo-router";
 export default function App() {
     return (
         <View style={styles.container}>
-            <Link href="/counter" style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
-                Go to counter
-            </Link>
-            {/* <Link href="/idea">Go to idea</Link> */}
             <ShoppingListItem name="Coffee" />
             <ShoppingListItem name="Tea" isCompleted />
             <ShoppingListItem name="Sugar" isCompleted />
